f749b50a61799214d7215c1debeca132fb7a5456
RahulKrishnan21 <159160710+RahulKrishnan21@users.noreply.github.com>
2025-11-07 11:51:30 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,32 +1,86 @@
 from tkinter import *
-import requests
+import math
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+reps=0
+timer=None
+# ---------------------------- TIMER RESET ------------------------------- # 
+def reset_timer():
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text,text="00:00")
+    title_label.config(text="Timer")
+    check_marks.config(text="")
+    global reps
+    reps=0
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    global reps
+    reps+=1
 
-def get_quote():
-    response=requests.get(url="https://api.kanye.rest")
-    response.raise_for_status()
+    work_sec=WORK_MIN*60
+    short_break_sec=SHORT_BREAK_MIN*60
+    long_break_sec=LONG_BREAK_MIN*60
+    
+    
+    if reps%8==0:
+        countdown(long_break_sec)
+        title_label.config(text="Break",fg=RED)
+    elif reps%2==0:
+        countdown(short_break_sec)
+        title_label.config(text="Break",fg=PINK)
+    else:
+        countdown(work_sec)
+        title_label.config(text="Work",fg=GREEN)
+        
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def countdown(count):
+    count_min=math.floor(count/60)
+    count_sec=count % 60
+    if count_sec<=10:
+        count_sec=f"0{count_sec}"
+    canvas.itemconfig(timer_text,text=f"{count_min}:{count_sec}")
+    if count>0:
+        global timer
+        timer=window.after(1000,countdown,count-1)
+    else:
+        start_timer()
+        marks=""
+        work_sessions=math.floor(reps/2)
+        for _ in range(work_sessions):
+            marks+="✔"
+        check_marks.config(text=marks)
 
-    data=response.json()
-    quote=data["quote"]
-    print(quote)
-    canvas.itemconfig(quote_text,text=quote)
-    #Write your code here.
+# ---------------------------- UI SETUP ------------------------------- #
+window=Tk()
+window.title("Pomodoro")
+
+window.config(padx=100,pady=50,bg=YELLOW)
 
 
-
-window = Tk()
-window.title("Kanye Says...")
-window.config(padx=50, pady=50)
-
-canvas = Canvas(width=300, height=414)
-background_img = PhotoImage(file="background.png")
-canvas.create_image(150, 207, image=background_img)
-quote_text = canvas.create_text(150, 207, text="Kanye Quote Goes HERE", width=250, font=("Arial", 30, "bold"), fill="white")
-canvas.grid(row=0, column=0)
-
-kanye_img = PhotoImage(file="kanye.png")
-kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
-kanye_button.grid(row=1, column=0)
+title_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50, "bold"))
+title_label.grid(column=1, row=0)
 
 
+canvas=Canvas(width=200,height=224,bg=YELLOW, highlightthickness=0)
+tomato_img=PhotoImage(file="tomato.png")
+canvas.create_image(100,112,image=tomato_img)
 
+timer_text=canvas.create_text(100,130,text="00:00",fill="white",font=(FONT_NAME,35,"bold"))
+canvas.grid(column=1,row=1)
+
+start_button=Button(text="Start",highlightthickness=0,command=start_timer)
+start_button.grid(column=0,row=2)
+
+reset_button=Button(text="Reset",highlightthickness=0,command=reset_timer)
+reset_button.grid(column=2,row=2)
+
+check_marks=Label(text="",fg=GREEN,bg=YELLOW)
+check_marks.grid(column=1,row=3)
 window.mainloop()
