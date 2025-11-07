8d0b41310a673c677c068d938c55e6be03096702
RahulKrishnan21 <159160710+RahulKrishnan21@users.noreply.github.com>
2025-11-07 11:53:04 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,86 +1,49 @@
-from tkinter import *
-import math
-# ---------------------------- CONSTANTS ------------------------------- #
-PINK = "#e2979c"
-RED = "#e7305b"
-GREEN = "#9bdeac"
-YELLOW = "#f7f5dd"
-FONT_NAME = "Courier"
-WORK_MIN = 25
-SHORT_BREAK_MIN = 5
-LONG_BREAK_MIN = 20
-reps=0
-timer=None
-# ---------------------------- TIMER RESET ------------------------------- # 
-def reset_timer():
-    window.after_cancel(timer)
-    canvas.itemconfig(timer_text,text="00:00")
-    title_label.config(text="Timer")
-    check_marks.config(text="")
-    global reps
-    reps=0
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
-def start_timer():
-    global reps
-    reps+=1
+from turtle import Turtle, Screen
+from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
+import time
+screen=Screen()
+screen.bgcolor("black")
+screen.setup(width=800,height=600)
+screen.title("Pong")
+screen.tracer(0)
 
-    work_sec=WORK_MIN*60
-    short_break_sec=SHORT_BREAK_MIN*60
-    long_break_sec=LONG_BREAK_MIN*60
+r_paddle=Paddle((350,0))
+l_paddle=Paddle((-350,0))
+ball=Ball()
+scoreboard=Scoreboard()
+screen.listen()
+
+
+screen.onkey(r_paddle.go_up,"Up")
+screen.onkey(r_paddle.go_down,"Down")
+screen.onkey(l_paddle.go_up,"w")
+screen.onkey(l_paddle.go_down,"s")
+
+game_is_on= True
+while game_is_on:
+    time.sleep(ball.move_speed)
+    screen.update()
+    ball.move()
+
+    #detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        #needs to bounce
+        ball.bounce_y()
     
+    #detect collison with right paddle
+    if ball.distance(r_paddle)<50 and ball.xcor()>320 or ball.distance(l_paddle)<50 and ball.xcor()<-320:
+        ball.bounce_x()
     
-    if reps%8==0:
-        countdown(long_break_sec)
-        title_label.config(text="Break",fg=RED)
-    elif reps%2==0:
-        countdown(short_break_sec)
-        title_label.config(text="Break",fg=PINK)
-    else:
-        countdown(work_sec)
-        title_label.config(text="Work",fg=GREEN)
-        
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
-def countdown(count):
-    count_min=math.floor(count/60)
-    count_sec=count % 60
-    if count_sec<=10:
-        count_sec=f"0{count_sec}"
-    canvas.itemconfig(timer_text,text=f"{count_min}:{count_sec}")
-    if count>0:
-        global timer
-        timer=window.after(1000,countdown,count-1)
-    else:
-        start_timer()
-        marks=""
-        work_sessions=math.floor(reps/2)
-        for _ in range(work_sessions):
-            marks+="✔"
-        check_marks.config(text=marks)
+    #detect R misses
+    if ball.xcor()>380:
+        ball.reset_position()
+        scoreboard.l_point()
+    
+    #detect L misses
+    if ball.xcor()<-380:
+        ball.reset_position()
+        scoreboard.r_point()
 
-# ---------------------------- UI SETUP ------------------------------- #
-window=Tk()
-window.title("Pomodoro")
-
-window.config(padx=100,pady=50,bg=YELLOW)
-
-
-title_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50, "bold"))
-title_label.grid(column=1, row=0)
-
-
-canvas=Canvas(width=200,height=224,bg=YELLOW, highlightthickness=0)
-tomato_img=PhotoImage(file="tomato.png")
-canvas.create_image(100,112,image=tomato_img)
-
-timer_text=canvas.create_text(100,130,text="00:00",fill="white",font=(FONT_NAME,35,"bold"))
-canvas.grid(column=1,row=1)
-
-start_button=Button(text="Start",highlightthickness=0,command=start_timer)
-start_button.grid(column=0,row=2)
-
-reset_button=Button(text="Reset",highlightthickness=0,command=reset_timer)
-reset_button.grid(column=2,row=2)
-
-check_marks=Label(text="",fg=GREEN,bg=YELLOW)
-check_marks.grid(column=1,row=3)
-window.mainloop()
+screen.exitonclick()
